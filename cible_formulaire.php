526ca86a0575769c269a46da49c8453465464d4e
FOURNIER <f2e@f2esystem.com>
2020-02-05 13:05:25 +0100

    Site Ini

---
+++ cible_formulaire.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Merci pour votre message</title>
+<style type="text/css">
+#apDiv1 {
+	position:absolute;
+	left:172px;
+	top:752px;
+	width:179px;
+	height:19px;
+	z-index:1;
+}
+body {
+	background-color: #E2E2E2;
+}
+.Titre {
+	color: #333;
+}
+.sous_titre {
+	font-size: 12px;
+}
+.sous_titre {
+	font-size: 14px;
+}
+.sous_titre {
+	font-family: Verdana, Geneva, sans-serif;
+}
+.sous_titre {
+	color: #999;
+}
+.sous_titre {
+	color: #666;
+}
+.sous_titre {
+	font-style: italic;
+}
+.Titre {
+	font-weight: bold;
+}
+titre {
+	font-size: 18px;
+}
+</style>
+<script type="text/javascript">
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+</script>
+<link href="titre.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+a:link {
+	color: #000;
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+	color: #000;
+}
+a:hover {
+	text-decoration: underline;
+	color: #999;
+}
+a:active {
+	text-decoration: none;
+}
+</style>
+</head>
+
+<body onload="MM_preloadImages('images/f2e_menu_societe_roll.png','images/f2e_menu_metiers_roll.png','images/f2e_menu_recherche_roll.png','images/f2e_menu_etudes_roll.png','images/f2e_menu_production_roll.png','images/f2e_menu_produits_roll.png','images/f2e_menu_support_roll.png','images/f2e_menu_actualite_roll.png','images/f2e_menu_contact_roll.png')">
+<table width="1024" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td><img src="images/f2e_ban.jpg" width="1024" height="109" /></td>
+  </tr>
+</table>
+<table width="1024" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="108"><a href="societe.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Societe','','images/f2e_menu_societe_roll.png',1)"><img src="images/f2e_menu_societe.png" name="Societe" width="108" height="34" border="0" id="Societe" /></a></td>
+    <td width="107"><a href="metiers.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Metier','','images/f2e_menu_metiers_roll.png',1)"><img src="images/f2e_menu_metiers.png" name="Metier" width="107" height="34" border="0" id="Metier" /></a></td>
+    <td width="125"><a href="recherche.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Recherche','','images/f2e_menu_recherche_roll.png',1)"><img src="images/f2e_menu_recherche.png" name="Recherche" width="125" height="34" border="0" id="Recherche" /></a></td>
+    <td width="91"><a href="etudes.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Etudes','','images/f2e_menu_etudes_roll.png',1)"></a><a href="etudes.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('etudes','','images/f2e_menu_etudes_roll.png',1)"><img src="images/f2e_menu_etudes.png" name="etudes" width="91" height="34" border="0" id="etudes" /></a></td>
+    <td width="137"><a href="production.html" onmouseout="MM_swapImgRestore()" onmouseover= "MM_swapImage('Production','','images/f2e_menu_production_roll.png',1)"><img src="images/f2e_menu_production.png" name="Production" width="137" height="34" border="0" id="Etudes" /></a></td>
+    <td width="113"><a href="produits.html" onmouseout="MM_swapImgRestore()" onmouseover= "MM_swapImage('Produits1','','images/f2e_menu_produits_roll.png',1)"><img src="images/f2e_menu_produits.png" name="Produits1" width="113" height="34" border="0" id="Produits1" /></a></td>
+    <td width="108"><a href="support.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Support','','images/f2e_menu_support_roll.png',1)"><img src="images/f2e_menu_support.png" name="Support" width="108" height="34" border="0" id="Support" /></a></td>
+    <td width="126"><a href="actualite.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Actualité','','images/f2e_menu_actualite_roll.png',1)"><img src="images/f2e_menu_actualite.png" name="Actualité" width="126" height="34" border="0" id="Actualité" /></a></td>
+    <td width="109"><a href="contact.html" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('contact','','images/f2e_menu_contact_roll.png',1)"><img src="images/f2e_menu_contact.png" name="contact" width="109" height="34" border="0" id="contact" /></a></td>
+  </tr>
+</table>
+<table width="1024" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">
+  <tr>
+    <td align="center"><?php
+     $headers ='From: '.$_POST['email']."\n"; 
+     $headers .='Content-Type: text/plain; charset="iso-8859-1"'."\n"; 
+     $headers .='Content-Transfer-Encoding: 8bit'; 
+	 $message = $_POST['nom']."\n\n".$_POST['message']."\n\n".$_POST['adresse']."\n".$_POST['tel']."\n".$_POST['email'];
+
+     if(mail('contact@f2esystem.com', 'Question',$message, $headers)) 
+     { 
+          echo 'Le message a bien été envoyé'; 
+     } 
+     else 
+     { 
+          echo 'Le message n\'a pu être envoyé'; 
+     } 
+?>
+	<p><a href="index.html"> Retour </a></p>
+	</td>
+  </tr>
+</table>
+<table width="1024" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td><img src="images/f2e_bottom.jpg" width="1024" height="65" border="0" usemap="#Map" /></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+
+<map name="mail" id="mail">
+  <area shape="rect" coords="88,39,219,54" href="mailto:f2e@f2esystem.com" />
+</map>
+
+<map name="Map" id="Map">
+  <area shape="rect" coords="85,37,218,55" href="mailto:f2e@f2esystem.com" />
+  <area shape="rect" coords="736,38,870,53" href="skype:+33(0)493400046" />
+</map>
+</body>
+</html>
